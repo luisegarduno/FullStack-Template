fe076fb23e44eaf3c60f1a18e881617284cae7ec
Luis Garduno <luis.e.garduno@gmail.com>
2021-05-26 20:30:28 -0500

    - Small changes

--- ProjectRun.sh
+++ ProjectRun.sh
@@ -9,9 +9,9 @@ docker-compose up -d
 
 source backend/node/.env
 
-echo "---> Loading Fermii db..."
+echo "---> Loading Project db..."
 sudo ./backend/node/execution-tools/wait-until.sh "docker-compose exec -T -e MYSQL_PWD=${MYSQL_PASS} backend-db mysql -u ${MYSQL_USER} -D ${MYSQL_DB} -e 'select 1'" > /dev/null 2>&1 
-echo "---> Successfully loaded in Fermii db"
+echo "---> Successfully loaded in Project db"
 
 echo "---> Restarting Database & API containers"
 docker restart ChangeMe_backend-db_1
