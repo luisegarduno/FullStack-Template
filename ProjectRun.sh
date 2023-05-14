1521ad15df1d43dc234a07dfb797bf384b3e1a72
luisegarduno <luis.e.garduno@gmail.com>
2023-05-14 07:05:31 -0400

    - Added gh-pages

--- ProjectRun.sh
+++ ProjectRun.sh
@@ -10,7 +10,7 @@ docker-compose up -d
 source backend/node/.env
 
 echo "---> Loading Project db..."
-sudo ./backend/node/execution-tools/wait-until.sh "docker-compose exec -T -e MYSQL_PWD=${MYSQL_PASS} backend-db mysql -u ${MYSQL_USER} -D ${MYSQL_DB} -e 'select 1'" > /dev/null 2>&1 
+sudo ./backend/node/execution-tools/wait-until.sh "docker-compose exec -T -e MYSQL_PWD=${MYSQL_PASS} ${MYSQL_HOST} mysql -u ${MYSQL_USER} -D ${MYSQL_DB} -e 'select 1'" > /dev/null 2>&1 
 echo "---> Successfully loaded in Project db"
 
 echo "---> Restarting Database & API containers"
